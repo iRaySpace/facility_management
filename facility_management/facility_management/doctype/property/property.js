73d1af54783212ac0f5aaa9de084f169990a8f9e
Ivan Ray Altomera <irayspacii@gmail.com>
2020-04-13 15:28:48 +0800

    feat(Property): added insurance details

--- facility_management/facility_management/doctype/property/property.js
+++ facility_management/facility_management/doctype/property/property.js
@@ -2,7 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Property', {
-	refresh: function(frm) {
-
+	insured: function(frm) {
+		const reqd_fields = ['insurance_company', 'insurance_type', 'insurance_start_date', 'insurance_end_date'];
+		reqd_fields.forEach((reqd_field) => frm.set_df_property(reqd_field, 'reqd', frm.doc.insured));
 	}
 });
