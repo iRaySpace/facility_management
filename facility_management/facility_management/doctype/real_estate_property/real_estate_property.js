39e16e9df25badcea6fca27d9212e899417406f9
Ivan Ray Altomera <irayspacii@gmail.com>
2020-04-13 15:42:11 +0800

    feat(Real Estate Property): added insurance details under Real Estate Property

--- facility_management/facility_management/doctype/real_estate_property/real_estate_property.js
+++ facility_management/facility_management/doctype/real_estate_property/real_estate_property.js
@@ -2,7 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Real Estate Property', {
-	refresh: function(frm) {
-
+	insured: function(frm) {
+		// TODO: commons
+		const reqd_fields = ['insurance_company', 'insurance_type', 'insurance_start_date', 'insurance_end_date'];
+		reqd_fields.forEach((reqd_field) => frm.set_df_property(reqd_field, 'reqd', frm.doc.insured));
 	}
 });
