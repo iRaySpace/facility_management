a35100f0bf1671dbf65ac09a6ca9fb470b9d6999
Ivan Ray Altomera <irayspacii@gmail.com>
2020-06-26 21:09:49 +0800

    fix: remove property rental type

--- facility_management/facility_management/doctype/property_rental_type/property_rental_type.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, 9T9IT and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Property Rental Type', {
-	refresh: function(frm) {
-
-	}
-});
