4a8d3d54cc5e9c1f829902ce07e1aeb9f60e45f9
Ivan Ray Altomera <irayspacii@gmail.com>
2020-06-30 17:45:05 +0800

    feat(ewa_billing): calculations

--- facility_management/facility_management/doctype/ewa_billing/ewa_billing.py
+++ facility_management/facility_management/doctype/ewa_billing/ewa_billing.py
@@ -6,5 +6,13 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+from functools import reduce
+
+
 class EWABilling(Document):
-	pass
+	def validate(self):
+		_validate_total(self)
+
+
+def _validate_total(billing):
+	billing.total = reduce(lambda total, x: total + x.excess_amount, billing.items, 0.00)
