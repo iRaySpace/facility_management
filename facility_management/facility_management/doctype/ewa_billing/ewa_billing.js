56b1ec7ae9e2414b3c5c14ca54da477c2182373a
Ivan Ray Altomera <irayspacii@gmail.com>
2020-06-30 17:03:25 +0800

    feat(ewa_billing): form behaviors

--- facility_management/facility_management/doctype/ewa_billing/ewa_billing.js
+++ facility_management/facility_management/doctype/ewa_billing/ewa_billing.js
@@ -3,12 +3,16 @@
 
 frappe.ui.form.on('EWA Billing', {
 	refresh: function(frm) {
-        _set_posting_date_time(frm);
+	    _set_frm_props(frm);
 	},
-	set_posting_time: function(frm) {
-	    _set_posting_date_time(frm);
-	},
+	set_posting_time: _set_posting_date_time,
 });
+
+
+function _set_frm_props(frm) {
+    _set_posting_date_time(frm);
+    frm.set_df_property('company', 'hidden', 1);
+}
 
 
 function _set_posting_date_time(frm) {
