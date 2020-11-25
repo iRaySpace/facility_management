ce30eee981b7ee7c97fa3eedf6ccfca06d9193fe
Ivan Ray Altomera <irayspacii@gmail.com>
2020-11-25 21:45:22 +0800

    feat(real_estate_property): added abbr

--- facility_management/facility_management/doctype/real_estate_property/real_estate_property.py
+++ facility_management/facility_management/doctype/real_estate_property/real_estate_property.py
@@ -4,7 +4,23 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
+
 class RealEstateProperty(Document):
-	pass
+	def validate(self):
+		_validate_abbr(self)
+
+
+def _validate_abbr(property):
+	if not property.abbr:
+		property.abbr = ''.join([c[0] for c in property.name.split()]).upper()
+
+	property.abbr = property.abbr.strip()
+
+	if not property.abbr:
+		frappe.throw(_("Abbreviation is mandatory"))
+
+	if frappe.get_all('Real Estate Property', filters=[['abbr', '=', property.abbr], ['name', '!=', property.name]]):
+		frappe.throw(_("Abbreviation already used for another property"))
