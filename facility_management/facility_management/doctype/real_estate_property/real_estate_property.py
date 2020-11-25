4ef77849bbd25142abc39caa0962d04246fac5dc
Ivan Ray Altomera <irayspacii@gmail.com>
2020-11-25 21:45:58 +0800

    chore: pretty code

--- facility_management/facility_management/doctype/real_estate_property/real_estate_property.py
+++ facility_management/facility_management/doctype/real_estate_property/real_estate_property.py
@@ -9,18 +9,21 @@ from frappe.model.document import Document
 
 
 class RealEstateProperty(Document):
-	def validate(self):
-		_validate_abbr(self)
+    def validate(self):
+        _validate_abbr(self)
 
 
 def _validate_abbr(property):
-	if not property.abbr:
-		property.abbr = ''.join([c[0] for c in property.name.split()]).upper()
+    if not property.abbr:
+        property.abbr = "".join([c[0] for c in property.name.split()]).upper()
 
-	property.abbr = property.abbr.strip()
+    property.abbr = property.abbr.strip()
 
-	if not property.abbr:
-		frappe.throw(_("Abbreviation is mandatory"))
+    if not property.abbr:
+        frappe.throw(_("Abbreviation is mandatory"))
 
-	if frappe.get_all('Real Estate Property', filters=[['abbr', '=', property.abbr], ['name', '!=', property.name]]):
-		frappe.throw(_("Abbreviation already used for another property"))
+    if frappe.get_all(
+        "Real Estate Property",
+        filters=[["abbr", "=", property.abbr], ["name", "!=", property.name]],
+    ):
+        frappe.throw(_("Abbreviation already used for another property"))
