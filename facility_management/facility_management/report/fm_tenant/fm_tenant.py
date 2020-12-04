7841dd5af47fd313209a917f224d69354f9f7e69
Ivan Ray Altomera <irayspacii@gmail.com>
2020-12-04 22:42:03 +0800

    feat(fm_tenant): update

--- facility_management/facility_management/report/fm_tenant/fm_tenant.py
+++ facility_management/facility_management/report/fm_tenant/fm_tenant.py
@@ -21,52 +21,56 @@ def _get_columns(filters):
         }
 
     return [
-        make_column("Tenant", "tenant", 130, "Link", "Tenant Master"),
-        make_column("Property Occupied", "property_occupied", 180, "Link", "Property"),
-        make_column("Lease Start Date", "lease_start_date", 130, "Date"),
-        make_column("Lease End Date", "lease_end_date", 130, "Date"),
-        make_column("Monthly Rental", "monthly_rental", 110, "Currency"),
-        make_column("Unpaid Amount", "unpaid_amount", 110, "Currency"),
-        make_column("Employer", "employer_name", 130),
-        make_column("Mobile", "mobile_no", 90),
-        make_column("Mobile 2", "mobile_no_2", 90),
-        make_column("Email", "email", 110),
+        make_column("Name of the Property", "property_group", 180, "Link", "Real Estate Property"),
+        make_column("Property Number", "property_no", 130),
+        make_column("Tenant", "tenant", 280, "Link", "Tenant Master"),
+        make_column("Mobile", "mobile_no", 110),
+        make_column("Mobile 2", "mobile_no_2", 110),
+        make_column("Email", "email", 210),
+        make_column("CPR/CR No", "cr_no", 180),
+        make_column("Contract Period Start", "contract_start_date", 130, "Date"),
+        make_column("Contract Period End", "contract_end_date", 130, "Date"),
+        make_column("Security Amount", "security_amount", 130, "Currency"),
+        make_column("EWA Limit", "ewa_limit", 130, "Currency"),
+        make_column("Furnished", "furnished", 130, "Data")
     ]
 
 
 def _get_data(filters):
     def make_data(renting):
-        rental_amount = renting.get("rental_amount")
-        rental_frequency = renting.get("rental_frequency")
-        monthly_rental = _get_monthly_rental(rental_amount, rental_frequency)
         return {
+            "property_group": renting.get("property_group"),
+            "property_no": renting.get("property_no"),
             "tenant": renting.get("tenant"),
-            "property_occupied": renting.get("property"),
-            "lease_start_date": renting.get("contract_start_date"),
-            "lease_end_date": renting.get("contract_end_date"),
-            "monthly_rental": monthly_rental,
-            "unpaid_amount": 0.00,
-            "employer_name": renting.get("employer_name"),
             "mobile_no": renting.get("mobile_no"),
             "mobile_no_2": renting.get("mobile_no_2"),
             "email": renting.get("email"),
+            "cr_no": renting.get("cr_no"),
+            "contract_start_date": renting.get("contract_start_date"),
+            "contract_end_date": renting.get("contract_end_date"),
+            "security_amount": renting.get("deposit_amount"),
+            "ewa_limit": renting.get("ewa_limit"),
+            "furnished": renting.get("furnished")
         }
 
     tenant_rentings = frappe.db.sql(
         """
         SELECT 
+            tr.property_group,
+            p.property_no,
             tr.tenant,
-            tr.property,
-            tr.contract_start_date,
-            tr.contract_end_date,
-            tr.rental_frequency,
-            tr.rental_amount,
-            t.employer_name,
             t.mobile_no,
             t.mobile_no_2,
-            t.email
+            t.email,
+            t.cr_no,
+            tr.contract_start_date,
+            tr.contract_end_date,
+            tr.deposit_amount,
+            tr.ewa_limit,
+            p.furnished
         FROM `tabRental Contract` tr
-        LEFT JOIN `tabTenant Master` t ON tr.tenant =  t.name
+        LEFT JOIN `tabTenant Master` t ON tr.tenant = t.name
+        LEFT JOIN `tabProperty` p ON tr.property = p.name
         """,
         as_dict=True,
     )
