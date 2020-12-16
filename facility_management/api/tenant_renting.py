aba02c0633a585f52fbe51ee42cf0a626f2c5b47
Ivan Ray Altomera <irayspacii@gmail.com>
2020-12-16 22:45:18 +0800

    chore: prettify tenant_renting

--- facility_management/api/tenant_renting.py
+++ facility_management/api/tenant_renting.py
@@ -7,15 +7,15 @@ from frappe.utils.data import nowdate
 @frappe.whitelist()
 def get_rental_listing():
     def data_occupied(rental):
-        name = rental.get('name')
-        return {'occupied': name in rented_properties}
+        name = rental.get("name")
+        return {"occupied": name in rented_properties}
 
     rented_properties = _get_rented_properties()
     occupied = list(map(data_occupied, _get_rental_properties()))
 
     return {
-        'Vacant': len(list(filter(lambda x: not x['occupied'], occupied))),
-        'Occupied': len(list(filter(lambda x: x['occupied'], occupied)))
+        "Vacant": len(list(filter(lambda x: not x["occupied"], occupied))),
+        "Occupied": len(list(filter(lambda x: x["occupied"], occupied))),
     }
 
 
@@ -26,33 +26,39 @@ def get_customer(tenant_renting):
     :param tenant_renting:
     :return:
     """
-    tenant = frappe.db.get_value('Rental Contract', tenant_renting, 'tenant')
-    return frappe.db.get_value('Tenant Master', tenant, 'customer')
+    tenant = frappe.db.get_value("Rental Contract", tenant_renting, "tenant")
+    return frappe.db.get_value("Tenant Master", tenant, "customer")
 
 
 def _get_rental_properties():
-    return frappe.db.sql("""
-		SELECT
-			p.name,
-			p.title,
-			p.property_type
-		FROM `tabProperty` p
-		WHERE p.property_status = 'Rental'
-	""", as_dict=True)
+    return frappe.db.sql(
+        """
+            SELECT
+                p.name,
+                p.title,
+                p.property_type
+            FROM `tabProperty` p
+            WHERE p.property_status = 'Rental'
+        """,
+        as_dict=True
+    )
 
 
 def _get_rented_properties():
     def make_data(tenant_renting):
-        return tenant_renting.get('property')
+        return tenant_renting.get("property")
 
     return list(
         map(
             make_data,
-            frappe.db.sql("""
-				SELECT property 
-				FROM `tabTenant Renting`
-				WHERE %s 
-				BETWEEN contract_start_date AND contract_end_date
-			""", nowdate(), as_dict=True)
+            frappe.db.sql(
+                """
+                    SELECT property 
+                    FROM `tabTenant Renting`
+                    WHERE %s BETWEEN contract_start_date AND contract_end_date
+                """,
+                nowdate(),
+                as_dict=True,
+            ),
         )
     )
