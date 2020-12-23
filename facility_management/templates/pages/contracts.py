3fdc69fff8008ddf64c28fddfa8f55d2a17f11a4
Ivan Ray Altomera <irayspacii@gmail.com>
2020-12-23 21:37:58 +0800

    feat(contracts): based on user

--- facility_management/templates/pages/contracts.py
+++ facility_management/templates/pages/contracts.py
@@ -1,5 +1,5 @@
 import frappe
-import json
+from toolz import first
 
 
 def get_context(context):
@@ -10,13 +10,15 @@ def get_context(context):
     if name:
         context.contract = frappe.get_doc("Rental Contract", name)
     else:
-        context.contracts = _get_contracts()
+        tenant = _get_tenant(_get_customer(frappe.session.user))
+        context.contracts = _get_contracts(tenant)
 
 
-def _get_contracts():
+def _get_contracts(tenant):
     return frappe.get_all(
         "Rental Contract",
         fields=["name", "status", "property", "posting_date"],
+        filters={"tenant": tenant},
     )
 
 
@@ -37,3 +39,25 @@ def _get_contract(name):
         ],
         as_dict=1,
     )
+
+
+def _get_customer(user):
+    customer = frappe.db.sql(
+        """
+            SELECT dl.link_name
+            FROM `tabContact` c
+            INNER JOIN `tabDynamic Link` dl ON dl.parent = c.name
+            WHERE dl.link_doctype = "Customer" AND c.user = %s
+        """,
+        user,
+        as_dict=1
+    )
+    return first(customer).get("link_name") if customer else None
+
+
+def _get_tenant(customer):
+    tenant = frappe.get_all(
+        "Tenant Master",
+        filters={"customer": customer}
+    )
+    return first(tenant).get("name") if tenant else None
