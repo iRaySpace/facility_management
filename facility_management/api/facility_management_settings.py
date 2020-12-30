1083d22959d9e86a6d02d771c9e528467183fde4
Ivan Ray Altomera <irayspacii@gmail.com>
2020-12-30 19:22:31 +0800

    feat(facility_management_settings): added pages to portal

--- facility_management/api/facility_management_settings.py
+++ facility_management/api/facility_management_settings.py
@@ -9,3 +9,54 @@ def add_dashboard():
         dashboard.append('charts', {'chart': chart})
     dashboard.save()
     return dashboard
+
+
+@frappe.whitelist()
+def add_pages_to_portal():
+    portal_settings = frappe.get_doc('Portal Settings', 'Portal Settings')
+    menu = [
+        {
+            'title': 'Contracts',
+            'enabled': True,
+            'route': '/contracts',
+            'reference_doctype': 'Rental Contract',
+            'role': 'Customer',
+        },
+        {
+            'title': 'Violations',
+            'enabled': True,
+            'route': '/violations',
+            'reference_doctype': 'Tenant Violation',
+            'role': 'Customer',
+        },
+        {
+            'title': 'Maintenance List',
+            'enabled': True,
+            'route': '/maintenances',
+            'reference_doctype': 'Property Maintenance',
+            'role': 'Customer',
+        },
+        {
+            'title': 'New Maintenance',
+            'enabled': True,
+            'route': '/new-maintenance',
+            'reference_doctype': 'Property Maintenance',
+            'role': 'Customer',
+        },
+        {
+            'title': 'Receipts',
+            'enabled': True,
+            'route': '/receipts',
+            'reference_doctype': 'Payment Entry',
+            'role': 'Customer',
+        },
+        {
+            'title': 'Personal Information',
+            'enabled': True,
+            'route': '/personal-information',
+        },
+    ]
+    for row in menu:
+        portal_settings.append('menu', row)
+    portal_settings.save()
+    return True
