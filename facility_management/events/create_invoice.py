3194b9226c7881ee82c7bedff59ca970e9121938
Ivan Ray Altomera <irayspacii@gmail.com>
2021-01-07 17:46:16 +0800

    refactor(black): format code

--- facility_management/events/create_invoice.py
+++ facility_management/events/create_invoice.py
@@ -6,12 +6,14 @@ from facility_management.helpers import set_invoice_created
 
 # TODO: remove unused codes
 def execute(**kwargs):
-    rental_contract = kwargs.get('rental_contract', None)
-    rental_contract_items = kwargs.pop('rental_contract_items', None)
+    rental_contract = kwargs.get("rental_contract", None)
+    rental_contract_items = kwargs.pop("rental_contract_items", None)
 
     tenant_dues = _get_tenant_dues(kwargs)
-    rental_item = frappe.db.get_single_value('Facility Management Settings', 'rental_item')
-    submit_si = frappe.db.get_single_value('Facility Management Settings', 'submit_si')
+    rental_item = frappe.db.get_single_value(
+        "Facility Management Settings", "rental_item"
+    )
+    submit_si = frappe.db.get_single_value("Facility Management Settings", "submit_si")
 
     if not tenant_dues and rental_contract_items:
         tenant_dues = rental_contract_items
@@ -20,40 +22,49 @@ def execute(**kwargs):
         if not isinstance(tenant_due, _dict):
             tenant_due = tenant_due.as_dict()
 
-        tenant = tenant_due.get('tenant')
-        description = tenant_due.get('description')
-        rental_amount = tenant_due.get('rental_amount')
-        advance_paid_amount = tenant_due.get('advance_paid_amount')
-        customer = frappe.db.get_value('Tenant Master', tenant, 'customer')
+        tenant = tenant_due.get("tenant")
+        description = tenant_due.get("description")
+        rental_amount = tenant_due.get("rental_amount")
+        advance_paid_amount = tenant_due.get("advance_paid_amount")
+        customer = frappe.db.get_value("Tenant Master", tenant, "customer")
 
-        parent_rc = tenant_due.get('parent')
+        parent_rc = tenant_due.get("parent")
         if not parent_rc:
             parent_rc = rental_contract
 
-        amount = advance_paid_amount if description == 'Advance Payment' else rental_amount
+        amount = (
+            advance_paid_amount if description == "Advance Payment" else rental_amount
+        )
 
-        invoice = frappe.new_doc('Sales Invoice')
-        invoice.update({
-            'customer': customer,
-            'posting_date': get_first_day(tenant_due.get('invoice_date')),
-            'posting_time': 0,
-            'due_date': tenant_due.get('invoice_date'),
-            'debit_to': frappe.db.get_value('Company', invoice.company, 'default_receivable_account'),
-            'set_posting_time': 1,
-            'pm_rental_contract': parent_rc
-        })
-        invoice.append('items', {
-            'item_code': rental_item,
-            'rate': amount,
-            'qty': 1.0,
-        })
+        invoice = frappe.new_doc("Sales Invoice")
+        invoice.update(
+            {
+                "customer": customer,
+                "posting_date": get_first_day(tenant_due.get("invoice_date")),
+                "posting_time": 0,
+                "due_date": tenant_due.get("invoice_date"),
+                "debit_to": frappe.db.get_value(
+                    "Company", invoice.company, "default_receivable_account"
+                ),
+                "set_posting_time": 1,
+                "pm_rental_contract": parent_rc,
+            }
+        )
+        invoice.append(
+            "items",
+            {
+                "item_code": rental_item,
+                "rate": amount,
+                "qty": 1.0,
+            },
+        )
         invoice.set_missing_values()
         invoice.save()
 
         if submit_si:
             invoice.submit()
 
-        set_invoice_created(tenant_due.get('name'), invoice.name)
+        set_invoice_created(tenant_due.get("name"), invoice.name)
 
 
 def _get_tenant_dues(filters):
@@ -78,19 +89,18 @@ def _get_tenant_dues(filters):
             WHERE rc.docstatus = 1 
             AND rci.is_invoice_created = 0
             {clauses}
-        """.format(clauses='AND ' + clauses if clauses else ''),
-        {
-            **filters,
-            'now': get_last_day(today())
-        },
-        as_dict=True
+        """.format(
+            clauses="AND " + clauses if clauses else ""
+        ),
+        {**filters, "now": get_last_day(today())},
+        as_dict=True,
     )
 
 
 def _get_clauses(filters):
     clauses = []
-    if filters.get('rental_contract'):
-        clauses.append('rc.name = %(rental_contract)s')
-    if not filters.get('apply_now'):
-        clauses.append('rci.invoice_date < %(now)s')
-    return 'AND'.join(clauses)
+    if filters.get("rental_contract"):
+        clauses.append("rc.name = %(rental_contract)s")
+    if not filters.get("apply_now"):
+        clauses.append("rci.invoice_date < %(now)s")
+    return "AND".join(clauses)
