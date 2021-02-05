68b05b94c73b3032b2cda76363d85f77409bddb2
Ivan Ray Altomera <irayspacii@gmail.com>
2021-02-05 22:02:07 +0800

    fix(property): empty data error

--- facility_management/api/property.py
+++ facility_management/api/property.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe import _
 
 
 def get_property_name(property):
@@ -9,7 +10,11 @@ def get_property_name(property):
     address_line_1, city, country = frappe.get_value(
         "Real Estate Property", property_group, ["address_line_1", "city", "country"]
     )
-    city_country = ", ".join([city, country])
-    complete_address = " ".join([address_line_1, city_country])
+
+    try:
+        city_country = ", ".join([city, country])
+        complete_address = " ".join([address_line_1, city_country])
+    except:
+        frappe.throw(_("Please set your Real Estate Property {}".format(property_group)))
 
     return ", ".join([title, complete_address])
