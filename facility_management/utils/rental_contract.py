91909ec76be10078a3aca1e75f75c80345260876
Ivan Ray Altomera <irayspacii@gmail.com>
2021-02-05 22:05:26 +0800

    fix(rental_contract): make_description

--- facility_management/utils/rental_contract.py
+++ facility_management/utils/rental_contract.py
@@ -3,6 +3,7 @@ from facility_management.api.property import get_property_name
 
 def make_description(data):
     return " ".join([
+        "Rental for",
         data.get("posting_date").strftime("%b %Y"),
-        f"for {get_property_name(data.get('property'))} as per {data.get('rental_contract')}"
+        f"for property {get_property_name(data.get('property'))} as per contract {data.get('rental_contract')}"
     ])
