8912e5c0552e4c6816ed77f4aa10417a2b631d28
Ivan Ray Altomera <irayspacii@gmail.com>
2021-02-15 22:19:35 +0800

    chore(rental_contract): code format

--- facility_management/utils/rental_contract.py
+++ facility_management/utils/rental_contract.py
@@ -2,17 +2,21 @@ from facility_management.api.property import get_property_name
 
 
 def make_description(data):
-    return " ".join([
-        "Rental for",
-        data.get("posting_date").strftime("%b %Y"),
-        f"for property {get_property_name(data.get('property'))} as per contract {data.get('rental_contract')}"
-    ])
+    return " ".join(
+        [
+            "Rental for",
+            data.get("posting_date").strftime("%b %Y"),
+            f"for property {get_property_name(data.get('property'))} as per contract {data.get('rental_contract')}",
+        ]
+    )
 
 
 def make_item_description(data):
-    return " ".join([
-        "Rent of",
-        get_property_name(data.get('property')),
-        "for",
-        data.get("posting_date").strftime("%B, %Y"),
-    ])
+    return " ".join(
+        [
+            "Rent of",
+            get_property_name(data.get("property")),
+            "for",
+            data.get("posting_date").strftime("%B, %Y"),
+        ]
+    )
