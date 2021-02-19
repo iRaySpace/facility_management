daba322f3dcac40b8e84625d914655d84f8baf09
Ivan Ray Altomera <irayspacii@gmail.com>
2021-02-19 19:27:22 +0800

    feat(sales_invoice): update rental contract item description

--- facility_management/doc_events/sales_invoice.py
+++ facility_management/doc_events/sales_invoice.py
@@ -1,29 +1,51 @@
 import frappe
+from toolz import first
 
 
 def validate(doc, method):
     _set_missing_values(doc)
+    _set_rental_contract_item(doc)
 
 
 def _set_missing_values(invoice):
     if not invoice.customer_name:
-        invoice.customer_name = frappe.get_value('Customer', invoice.customer, 'customer_name')
+        invoice.customer_name = frappe.get_value(
+            "Customer", invoice.customer, "customer_name"
+        )
 
     if invoice.pm_rental_contract:
         rental_contract = frappe.get_all(
-            'Rental Contract',
-            filters={'name': invoice.pm_rental_contract},
-            fields=['tenant', 'property', 'property_group']
+            "Rental Contract",
+            filters={"name": invoice.pm_rental_contract},
+            fields=["tenant", "property", "property_group"],
         )[0]
-        property_group = rental_contract.get('property_group')
-        cost_center = frappe.get_value('Real Estate Property', property_group, 'cost_center')
+        property_group = rental_contract.get("property_group")
+        cost_center = frappe.get_value(
+            "Real Estate Property", property_group, "cost_center"
+        )
 
         invoice.pm_property_group = property_group
-        invoice.pm_tenant = rental_contract.get('tenant')
-        invoice.pm_property = rental_contract.get('property')
+        invoice.pm_tenant = rental_contract.get("tenant")
+        invoice.pm_property = rental_contract.get("property")
 
         if not invoice.remarks:
             invoice.remarks = invoice.pm_rental_contract
 
         for item in invoice.items:
             item.cost_center = cost_center
+
+
+def _set_rental_contract_item(doc):
+    rental_contract_items = frappe.get_all(
+        "Rental Contract Item",
+        filters={"parent": doc.pm_rental_contract, "invoice_date": doc.due_date},
+    )
+    if rental_contract_items:
+        rental_contract_item = first(rental_contract_items)
+        description = f"Rent {doc.status}"
+        frappe.db.set_value(
+            "Rental Contract Item",
+            rental_contract_item.get("name"),
+            "description",
+            description,
+        )
