d2f2c138a18826738f736ea01eec47ed2bfc06e5
Ivan Ray Altomera <irayspacii@gmail.com>
2021-02-19 19:26:59 +0800

    fix(rental_contract): getdate for item description

--- facility_management/utils/rental_contract.py
+++ facility_management/utils/rental_contract.py
@@ -1,3 +1,4 @@
+from frappe.utils.data import getdate
 from facility_management.api.property import get_property_name
 
 
@@ -17,6 +18,6 @@ def make_item_description(data):
             "Rent of",
             get_property_name(data.get("property")),
             "for",
-            data.get("posting_date").strftime("%B %Y"),
+            getdate(data.get("posting_date")).strftime("%B %Y"),
         ]
     )
